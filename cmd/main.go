e1c41b0b21256a5d35cb44df475a4088b668a02b
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2026-01-27 13:03:50 +0200

    increase agent-diagnostics-timeout default to 5 minutes

--- cmd/main.go
+++ cmd/main.go
@@ -51,7 +51,7 @@ func main() {
 	cmd.Flags().StringVar(&diagParams.Kubeconfig, "kubeconfig", "", "optional path to kube config, defaults to $HOME/.kube/config")
 	cmd.Flags().BoolVar(&diagParams.Verbose, "verbose", false, "Verbose mode")
 	cmd.Flags().DurationVar(&diagParams.StackDiagnosticsTimeout, "stack-diagnostics-timeout", 5*time.Minute, "Maximum time to wait for Elaticsearch and Kibana diagnostics to complete")
-	cmd.Flags().DurationVar(&diagParams.AgentDiagnosticsTimeout, "agent-diagnostics-timeout", 2*time.Minute, "Maximum time to wait for each Elastic Agent diagnostic to complete")
+	cmd.Flags().DurationVar(&diagParams.AgentDiagnosticsTimeout, "agent-diagnostics-timeout", 5*time.Minute, "Maximum time to wait for each Elastic Agent diagnostic to complete")
 
 	if err := cmd.MarkFlagRequired(resourcesNamespaces); err != nil {
 		exitWithError(err)
